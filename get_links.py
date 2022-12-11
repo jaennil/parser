587eeee4bff0eb39389a216892f5b3c8825820ba
johnil <ssushnost@gmail.com>
2022-12-11 23:00:30 +0300

    shit

--- get_links.py
+++ get_links.py
@@ -1,10 +1,10 @@
 from serpapi import GoogleSearch
 
-api_key = "fe3e1e6c843b7cd1f375a5ba0fc399a12cf7e594b0a3cb111d7bb90ab1724799"
+API_KEY="fe3e1e6c843b7cd1f375a5ba0fc399a12cf7e594b0a3cb111d7bb90ab1724799"
 params = {
     "engine": "google",
     "q": "Школа 2114 О нас",
-    "api_key": api_key,
+    "api_key": API_KEY,
 }
 
 search = GoogleSearch(params)
@@ -12,11 +12,3 @@ results = search.get_dict()
 
 for result in results["organic_results"]:
     print(f"Title: {result['title']}\nLink: {result['link']}\n")
-
-
-def asdf():
-    print(2)
-
-
-def asdf2():
-    ...
